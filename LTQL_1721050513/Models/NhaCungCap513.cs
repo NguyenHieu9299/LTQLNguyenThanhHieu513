fc2630f1d78d27a0e41471b4a5b0f93a7e358e7f
hieunguyen <hieunguyen@DESKTOP-O8TN4L4>
2021-12-21 13:47:52 +0700

    “Create_Table_NCC_NTH513

--- LTQL_1721050513/Models/NhaCungCap513.cs
+++ LTQL_1721050513/Models/NhaCungCap513.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LTQL_1721050513.Models
 {
     public class NhaCungCap513
     {
+        [Key]
+        [Display(Name = "Mã Nhà Cung Cấp")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MaNhaCungCap { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Tên Nhà Cung Cấp")]
 
+        public string TenNhaCungCap { get; set; }
+
+        
     }
 }
